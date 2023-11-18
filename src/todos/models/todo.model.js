0eac6ed17566dba9224814eba7983db2bc8fc884
Damian <a.damianrodriguez.93@gmail.com>
2023-11-18 18:45:45 -0300

    CheckList

---
+++ src/todos/models/todo.model.js
@@ -0,0 +1,16 @@
+import {v5 as uuid} from 'uuid';
+
+export class Todo {
+
+    /**
+     * 
+     * @param {String} description 
+     */
+    constructor(description) {
+        this.id = uuid();
+        this.description = description;
+        this.done = false;
+        this.createdat = new Date();
+    }
+
+}
